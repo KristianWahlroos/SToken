8e49862c9ad2fa78867af48ada738e1872cfa763
Kristian Wahlroos <kjhwah@utu.fi>
2019-05-30 23:10:50 +0300

    Create a token's functionality test script

--- interaction/interaction.js
+++ interaction/interaction.js
@@ -12,7 +12,7 @@ let abi = [
 ]
 
 //INSERT HERE YOUR OWN CONTRACT ADDRESS!
-const contractAddress = "0xC5e3ad48205A0BF14031E28732BE3Ef5c1F5F5ED";
+const contractAddress = "0x20dc1E6bb28eE94e39Fd7Df817eE8D2c6062027d";
 
 var signer0 = httpProvider.getSigner(0);
 var signer1 = httpProvider.getSigner(1);
@@ -26,38 +26,41 @@ let contractAsSigner1 = contract.connect(signer1);
 let contractAsSigner2 = contract.connect(signer2);
 let contractAsSigner3 = contract.connect(signer3);
 
-init();
-//testSlaves();
+testSlaves();
 
-function init(){
-    _setCooldownTime(1);
-    _createSlave(contractAsSigner0);
-    _createSlave(contractAsSigner1);
-    _createSlave(contractAsSigner2);
-    _createSlave(contractAsSigner3);
-    httpProvider.listAccounts().then(result => _transferFrom(contractAsSigner1, result[1],result[0], 6));
-    httpProvider.listAccounts().then(result => _getBalance(result, 0));
-    httpProvider.listAccounts().then(result => _getBalance(result, 1));
-    httpProvider.listAccounts().then(result => _getBalance(result, 2));
-    httpProvider.listAccounts().then(result => _getBalance(result, 3));
-}
-
-function testSlaves(){
-    // _work(contractAsSigner0, 0);
-    // _work(contractAsSigner1, 1);
-    _beFruitful(contractAsSigner0, 0, 1);
-    _beFruitful(contractAsSigner2, 2, 3);
-    //httpProvider.listAccounts().then(result => _transferFrom(contractAsSigner1, result[1],result[0], 6));
-    httpProvider.listAccounts().then(result => _getBalance(result, 0));
-    httpProvider.listAccounts().then(result => _getBalance(result, 1));
-    httpProvider.listAccounts().then(result => _getBalance(result, 2));
-    httpProvider.listAccounts().then(result => _getBalance(result, 3));
+async function testSlaves(){
+    await _setCooldownTime(0);
+    await _createSlave(contractAsSigner0);
+    await _createSlave(contractAsSigner1);
+    await _createSlave(contractAsSigner2);
+    await _createSlave(contractAsSigner3);
+    await httpProvider.listAccounts().then(result => _getBalance(result, 0));
+    await httpProvider.listAccounts().then(result => _getBalance(result, 1));
+    await httpProvider.listAccounts().then(result => _getBalance(result, 2));
+    await httpProvider.listAccounts().then(result => _getBalance(result, 3));
+    for(i = 0; i < 50; i++) {
+        await _beFruitful(contractAsSigner0, 0, 1);
+        await _beFruitful(contractAsSigner2, 2, 3);
+        await _work(contractAsSigner0, 0);
+        await _work(contractAsSigner1, 1);
+        await _work(contractAsSigner2, 2);
+        await httpProvider.listAccounts().then(result => _getBalance(result, 0));
+        await httpProvider.listAccounts().then(result => _getBalance(result, 1));
+        await httpProvider.listAccounts().then(result => _getBalance(result, 2));
+        await httpProvider.listAccounts().then(result => _getBalance(result, 3));
+    }
+    await httpProvider.listAccounts().then(result => _transferFrom(contractAsSigner1, result[1],result[0], 1));
+    await httpProvider.listAccounts().then(result => _transferFrom(contractAsSigner2, result[2],result[0], 2));
+    await httpProvider.listAccounts().then(result => _getBalance(result, 0));
+    await httpProvider.listAccounts().then(result => _getBalance(result, 1));
+    await httpProvider.listAccounts().then(result => _getBalance(result, 2));
+    await httpProvider.listAccounts().then(result => _getBalance(result, 3));
 }
 
 function _work(contractAsSigner, slaveId){
     var workPromise = contractAsSigner.work(slaveId);
     workPromise.then(function(tx) {
-        console.log(tx);
+        console.log("slave " + slaveId + " working!");
     })
     .catch((e) => console.log(e));
 }
@@ -65,7 +68,7 @@ function _work(contractAsSigner, slaveId){
 function _beFruitful(contractAsSigner, slaveId, targetId){
     var workPromise = contractAsSigner.beFruitful(slaveId, targetId);
     workPromise.then(function(tx) {
-        console.log(tx);
+        console.log("slave " + slaveId + " being fruitful with slave " + targetId + "!");
     })
     .catch((e) => console.log(e));
 }
@@ -73,7 +76,7 @@ function _beFruitful(contractAsSigner, slaveId, targetId){
 function _transferFrom(contractAsSigner, fromId, toId, slaveId){
     var workPromise = contractAsSigner.transferFrom(fromId, toId, slaveId);
     workPromise.then(function(tx) {
-        console.log(tx);
+        console.log("Trying to transfer slave " + slaveId);
     })
     .catch((e) => console.log(e));
 }
@@ -81,7 +84,7 @@ function _transferFrom(contractAsSigner, fromId, toId, slaveId){
 function _setCooldownTime(amount){
     var createSlavePromise = contractAsSigner0.setCooldownTime(amount);
     createSlavePromise.then(function(tx) {
-        console.log(tx);
+        console.log("Cooldown time is now " + amount + " in seconds");
     })
     .catch((e) => console.log(e));
 }
@@ -89,7 +92,7 @@ function _setCooldownTime(amount){
 function _createSlave(contractAsSigner){
     var createSlavePromise = contractAsSigner.createFirstSlave();
     createSlavePromise.then(function(tx) {
-        console.log(tx);
+        console.log("Creating a slave!");
     })
     .catch((e) => console.log(e));
 }
@@ -97,7 +100,7 @@ function _getBalance(result, accountId){
     var checkBalancePromise = contractAsSigner2.balanceOf(ethers.utils.getAddress(result[accountId]));
     checkBalancePromise
     .then(function(tx) {
-        console.log(tx);
+        console.log("User" + accountId + " has " + tx + " slaves");
     })
     .catch((e) => console.log(e));
 }
